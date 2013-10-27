428e359fd1458c3a8fb2d58bcfa6343786f03de8
Cristhian de la Fuente <cristhian.delafuente@gmail.com>
2013-10-26 20:36:48 -0500

    Added home page.

--- src/Jac/AboutUsBundle/Controller/DefaultController.php
+++ src/Jac/AboutUsBundle/Controller/DefaultController.php
@@ -1,13 +1,15 @@
 <?php
-
-namespace Jac\ProductsBundle\Controller;
+namespace Jac\AboutUsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JacProductsBundle:Default:index.html.twig');
+        return $this->render('JacAboutUsBundle:Default:index.html.twig');
     }
+
+
 }
---
+++ src/Jac/AboutUsBundle/JacAboutUsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jac\AboutUsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JacAboutUsBundle extends Bundle
+{
+}
